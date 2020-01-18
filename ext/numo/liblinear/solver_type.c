2cc754894c8687ea44f81b4af132ed46db37e6f9
yoshoku <yoshoku@outlook.com>
2020-01-19 08:53:27 +0900

    :art: Fix mixed tab and space indentation

--- ext/numo/liblinear/solver_type.c
+++ ext/numo/liblinear/solver_type.c
@@ -17,18 +17,18 @@ void rb_init_solver_type_module()
   rb_define_const(mSolverType, "L2R_L2LOSS_SVC", INT2NUM(L2R_L2LOSS_SVC));
   /* L2-regularized L1-loss support vector classification (dual) */
   rb_define_const(mSolverType, "L2R_L1LOSS_SVC_DUAL", INT2NUM(L2R_L1LOSS_SVC_DUAL));
-	/* support vector classification by Crammer and Singer */
+  /* support vector classification by Crammer and Singer */
   rb_define_const(mSolverType, "MCSVM_CS", INT2NUM(MCSVM_CS));
-	/* L1-regularized L2-loss support vector classification */
+  /* L1-regularized L2-loss support vector classification */
   rb_define_const(mSolverType, "L1R_L2LOSS_SVC", INT2NUM(L1R_L2LOSS_SVC));
-	/* L1-regularized logistic regression */
+  /* L1-regularized logistic regression */
   rb_define_const(mSolverType, "L1R_LR", INT2NUM(L1R_LR));
   /* L2-regularized logistic regression (dual) */
   rb_define_const(mSolverType, "L2R_LR_DUAL", INT2NUM(L2R_LR_DUAL));
-	/* L2-regularized L2-loss support vector regression (primal) */
+  /* L2-regularized L2-loss support vector regression (primal) */
   rb_define_const(mSolverType, "L2R_L2LOSS_SVR", INT2NUM(L2R_L2LOSS_SVR));
-	/* L2-regularized L2-loss support vector regression (dual) */
+  /* L2-regularized L2-loss support vector regression (dual) */
   rb_define_const(mSolverType, "L2R_L2LOSS_SVR_DUAL", INT2NUM(L2R_L2LOSS_SVR_DUAL));
-	/* L2-regularized L1-loss support vector regression (dual) */
+  /* L2-regularized L1-loss support vector regression (dual) */
   rb_define_const(mSolverType, "L2R_L1LOSS_SVR_DUAL", INT2NUM(L2R_L1LOSS_SVR_DUAL));
 }
