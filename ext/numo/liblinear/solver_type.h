9249c2cb255bb10a5080ed8027b7b1294d3f2235
yoshoku <yoshoku@outlook.com>
2022-01-09 08:49:14 +0900

    refactor!: remove existing native extension codes

--- ext/numo/liblinear/solver_type.h
+++
@@ -1,9 +0,0 @@
-#ifndef NUMO_LIBLINEAR_SOLVER_TYPE_H
-#define NUMO_LIBLINEAR_SOLVER_TYPE_H 1
-
-#include <linear.h>
-#include <ruby.h>
-
-void rb_init_solver_type_module();
-
-#endif /* NUMO_LIBLINEAR_SOLVER_TYPE_H */
